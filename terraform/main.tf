4683fc5f3621dffcba8c82d0c2e4372bd53a4be6
Conner Bartels <cbjr19@gmail.com>
2024-11-07 08:24:29 -0800

    Initial commit for infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.74.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami                     = "ami-0ddc798b3f1a5117e"
+  instance_type           = "t2.micro"
+  count                   = 2
+
+  tags = {
+    Name = "EC2 Terraform Instance"
+    ProvisionedBy = "Terraform"
+    Environment = "Prod"
+    Project = "Cloud Resume Challenge"
+  }
+}
+
+resource "aws_s3_bucket" "tatic_website_bucket" {
+    bucket = "connerbartels.com"
+
+    tags = {}
+        Name = "Test Static Website Bucket"
+        ProvisionedBy = "Terraform"
+        Environment = "Prod"
+        Project = "Cloud Resume Challenge"
+    }
+
+}
