8b25187e00279ed04bbf1ae217735be9b723d81b
Conner Bartels <cbjr19@gmail.com>
2024-11-07 11:17:15 -0800

    Add remote state S3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,42 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.74.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-conner-bartels"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "web_server" {
-  ami                     = "ami-0ddc798b3f1a5117e"
-  instance_type           = "t2.micro"
-  count                   = 2
+  ami             = "ami-0ddc798b3f1a5117e"
+  instance_type   = "t2.micro"
+  count           = 2
 
   tags = {
-    Name = "EC2 Terraform Instance"
+    Name          = "EC2 Terraform Instance"
     ProvisionedBy = "Terraform"
-    Environment = "Prod"
-    Project = "Cloud Resume Challenge"
+    Environment   = "Prod"
+    Project       = "Cloud Resume Challenge"
   }
 }
 
-resource "aws_s3_bucket" "tatic_website_bucket" {
-    bucket = "connerbartels.com"
+resource "aws_s3_bucket" "static_website_bucket" {
+  bucket = "connerbartels.com"
 
-    tags = {}
-        Name = "Test Static Website Bucket"
-        ProvisionedBy = "Terraform"
-        Environment = "Prod"
-        Project = "Cloud Resume Challenge"
-    }
-
+  tags = {
+    Name          = "Test Static Website Bucket"
+    ProvisionedBy = "Terraform"
+    Environment   = "Prod"
+    Project       = "Cloud Resume Challenge"
+  }
 }
